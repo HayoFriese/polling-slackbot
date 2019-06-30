8697ea322c03e908459f00aa89eb13f14d540c6d
Hayo Friese <hayo.friese@gmail.com>
2019-07-01 00:01:28 +0200

    no message

--- server/server.js
+++ server/server.js
@@ -8,6 +8,6 @@ const PORT = process.env.PORT || 5000;
 app.set('views', './views');
 
 app.use(require('./controllers'));
-app.listen(PORT);
+// app.listen(PORT);
 console.log(`listening on port ${PORT}`);
-// http.createServer(app).listen(PORT, () => console.log(`Listening on ${hostname}:${ PORT }`));
+http.createServer(app).listen(PORT, () => console.log(`Listening on ${hostname}:${ PORT }`));
